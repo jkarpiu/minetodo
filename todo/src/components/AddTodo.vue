df7fca35c32fa4f47aa4e78074a49a914f034c2c
Jakub Karp <karptutaj@gmail.com>
2020-03-29 18:11:04 +0200

    test

---
+++ todo/src/components/AddTodo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" name="title" v-model="title" placeholder="Dodaj zadanie...">
+            <input type="submit" value="Dodaj" class="btn">
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTodo(e){
+            e.preventDefault();
+            const newTodo = {
+                title: this.title,
+                completed: false
+            }
+            this.$emit('add-todo', newTodo);
+            this.title = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+    input[type="submit"] {
+        flex: 2;
+    }
+</style>
